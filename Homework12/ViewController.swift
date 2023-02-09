29c99c9bb6c43a2d817ebc1c70f172432648a916
Albina Lutfullina <106741510+albinalutfullina@users.noreply.github.com>
2023-02-10 01:32:44 +0500

    Initial Commit

---
+++ Homework12/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Homework12
+//
+//  Created by Альбина on 10.02.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
